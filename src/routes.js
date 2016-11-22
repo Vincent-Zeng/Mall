02b68ee291a3328a10f5f6f510f5dbb302d0b6bb
Draveness <stark.draven@gmail.com>
2016-11-22 23:19:18 +0800

    Add search place holder page

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,14 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import Home from './pages/home'
+import Search from './pages/customer/search'
 
 const routes = [{
   path: '/',
   component: Home
+}, {
+  path: '/search',
+  component: Search
 }]
 
 export default new VueRouter({
