5e467106096693cd84f9699d63306d99c6759801
Draveness <stark.draven@gmail.com>
2016-11-28 15:29:42 +0800

    Fixed signup service name

--- src/services/customer/signup.js
+++ src/services/customer/signup.js
@@ -1,4 +1,4 @@
-export const login = ({ telephone, name, password }) => {
+export const signup = ({ telephone, name, password }) => {
   return fetch(`/customer/register`, {
     method: 'POST',
     body: JSON.stringify({ "telephone": telephone, "password": password, "name": name })
