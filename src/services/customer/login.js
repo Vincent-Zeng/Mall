65b16a7c120549c18a94f988b0fe8941696a016e
Draveness <stark.draven@gmail.com>
2016-12-03 17:35:30 +0800

    Upload service in customer

--- src/services/customer/login.js
+++ src/services/customer/login.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-export const login = (telephone, password) => {
-  let body = JSON.stringify({ 'telephone': telephone, 'password': password })
+export const login = (email, password) => {
+  let body = JSON.stringify({ 'email': email, 'password': password })
   return Vue.http.post('/customer/login', body)
 }
